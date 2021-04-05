e2781ce7e557df89edffe773b529191647d322bc
Stephen Beattie <stephen.beattie@estimateone.com>
2021-04-05 22:11:36 +1000

    --wip-- [skip ci]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Card } from "components/Card";
+import redis from "redis";
 
 const App = (): JSX.Element => {
+  const redisClient = redis.createClient();
+  const pokeThree = redisClient.get("pokemon_3", function (err, reply) {
+    return JSON.parse(reply);
+  });
+
   return (
     <>
       <h1>Pokemon List</h1>
-      <Card />
+      <Card pokemon={pokeThree.valueOf} />
     </>
   );
 };
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,10 +2,10 @@ import { Image } from "components/Image";
 import React from "react";
 import styles from "./styles.module.scss";
 
-const Card: React.FC = () => (
+const Card: React.FC<{ pokemon: { name } }> = ({ pokemon }) => (
   <div className={styles.cardBorder}>
     <Image />
-    <h3>Sandslash</h3>
+    <h3>{pokemon.name}</h3>
   </div>
 );
 
