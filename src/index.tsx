3f9e9b6939a5d1540a05e39ba775bfebc88122ba
Stephen Beattie <stephen.beattie@estimateone.com>
2021-05-02 21:59:36 +1000

    Add sentry to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
+
+Sentry.init({
+  dsn:
+    "https://cf385b49f8b54cb3805fa0cf2f2d9b15@o605238.ingest.sentry.io/5744789",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
