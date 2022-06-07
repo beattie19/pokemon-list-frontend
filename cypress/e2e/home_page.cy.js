c1363eb78a326e8b106c8333d4029815cea5dc8b
Stephen Beattie <beattie19@hotmail.co.uk>
2022-06-08 00:47:11 +1000

    :construction: Attempt to add cypress intercept

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,7 +1,23 @@
+const { pokemonMock } = require("../../src/pokemonMock");
+
 describe('The Home Page', () => {
     it('successfully loads', () => {
+
+        cy.intercept(
+            'GET',
+            'https://fklqabeg8h.execute-api.ap-southeast-2.amazonaws.com/test/all-pokemon',
+            {
+              statusCode: 200,
+              body: {
+                message: 'Request successful',
+                result: ['my-data']
+              }
+            }
+          )
+          .as('typicode')
+
+    //   cy.intercept('GET', 'https://fklqabeg8h.execute-api.ap-southeast-2.amazonaws.com/test/all-pokemon', []).as('pokemons');
       cy.visit('http://localhost:3000')
       cy.get('input[name=searchBar]').type('Charizard')
     })
   })
-  
