e38b37d556ca5226b06d4301e4436077669cff88
Stephen Beattie <beattie19@hotmail.co.uk>
2022-07-02 06:26:28 -0700

    :wrench: Add user-event testing library package to allow easier testing of typing in tests, and add a basic unit  test for the SearchBar

---
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import SearchBar from '.';
+
+describe('<SearchBar>', () => {
+    test('renders correctly', async () => {
+        const mockOnChange = jest.fn();
+        const pokemonName = "Sandshrew";
+        render(<SearchBar searchTerm='' handleSearchTermChange={mockOnChange} />)
+
+        const searchBox = screen.getByRole('searchbox')
+        await userEvent.type(searchBox, pokemonName);
+
+        expect(mockOnChange).toBeCalledTimes(9);
+    })
+})
