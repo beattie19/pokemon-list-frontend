b8eb7a321b45ef61e0b741bb91768ff60ce79453
Stephen Beattie <beattie19@hotmail.co.uk>
2022-07-02 06:28:41 -0700

    :rotating_light: Fix linting issues

--- src/components/BaseStats/BaseStats.test.tsx
+++ src/components/BaseStats/BaseStats.test.tsx
@@ -3,25 +3,34 @@ import React from "react";
 import { BaseStats } from ".";
 
 const mockBaseStats = {
-    attack: 30,
-    specialAttack: 30,
-    defence: 30,
-    specialDefence: 30,
-    speed: 30,
-    hp: 30,
+  attack: 30,
+  specialAttack: 30,
+  defence: 30,
+  specialDefence: 30,
+  speed: 30,
+  hp: 30,
 };
 
 describe("<BaseStats /", () => {
-    test("Renders correctly", () => {
-        const expectedStatNamesInOrder = ['hp', 'attack', 'defence', 'sp. attack', 'sp. defense', 'speed'];
-    
-        render(<BaseStats baseStats={mockBaseStats} />)
-        const statListItems = screen.getAllByRole('listitem');
-        
-        expect(statListItems.length).toEqual(expectedStatNamesInOrder.length);
-        statListItems.map((stat, index) => {
-            console.log(expectedStatNamesInOrder[index]);
-            expect(stat).toHaveTextContent(new RegExp(expectedStatNamesInOrder[index],  'i'))
-        })
+  test("Renders correctly", () => {
+    const expectedStatNamesInOrder = [
+      "hp",
+      "attack",
+      "defence",
+      "sp. attack",
+      "sp. defense",
+      "speed",
+    ];
+
+    render(<BaseStats baseStats={mockBaseStats} />);
+    const statListItems = screen.getAllByRole("listitem");
+
+    expect(statListItems.length).toEqual(expectedStatNamesInOrder.length);
+    statListItems.map((stat, index) => {
+      console.log(expectedStatNamesInOrder[index]);
+      expect(stat).toHaveTextContent(
+        new RegExp(expectedStatNamesInOrder[index], "i")
+      );
     });
-})
+  });
+});
--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,17 +1,17 @@
-import { render, screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import React from 'react';
-import SearchBar from '.';
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import SearchBar from ".";
 
-describe('<SearchBar>', () => {
-    test('renders correctly', async () => {
-        const mockOnChange = jest.fn();
-        const pokemonName = "Sandshrew";
-        render(<SearchBar searchTerm='' handleSearchTermChange={mockOnChange} />)
+describe("<SearchBar>", () => {
+  test("renders correctly", async () => {
+    const mockOnChange = jest.fn();
+    const pokemonName = "Sandshrew";
+    render(<SearchBar searchTerm="" handleSearchTermChange={mockOnChange} />);
 
-        const searchBox = screen.getByRole('searchbox')
-        await userEvent.type(searchBox, pokemonName);
+    const searchBox = screen.getByRole("searchbox");
+    await userEvent.type(searchBox, pokemonName);
 
-        expect(mockOnChange).toBeCalledTimes(9);
-    })
-})
+    expect(mockOnChange).toBeCalledTimes(9);
+  });
+});
